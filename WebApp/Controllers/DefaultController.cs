f01bdfd89a9f7094465ee634171b2bfeb01b4b74
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2024-04-18 22:29:54 +0200

    dags å starta med footer

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -1,11 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
-public class DefaultController : Controller
+public class DefaultController(HttpClient httpClient) : Controller
 {
+    private readonly HttpClient _httpClient = httpClient;
+
     public IActionResult Home()
     {
         return View();
+    }
+
+
+    public async Task<IActionResult> Subscribe(SubscribeViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:7107/api/subscribe", content);
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["StatusMessage"] = "you are now subscribed";
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+            {
+                TempData["StatusMessage"] = "you are already subscribed";
+            }
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Invalid email address";
+        }
+
+        return RedirectToAction("Home", "Default", "subscribe");
     }
 }
