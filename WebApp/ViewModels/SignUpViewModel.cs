f5ac4ed44f70fb46ae589f7579a91d84dfc2a5aa
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2024-04-21 22:04:44 +0200

    java fixad

--- WebApp/ViewModels/SignUpViewModel.cs
+++ WebApp/ViewModels/SignUpViewModel.cs
@@ -7,26 +7,30 @@ public class SignUpViewModel
 {
 
 
-
+    [DataType(DataType.Text)]
     [Display(Name = "First name", Prompt = "Enter your first name")]
-    [Required]
+    [Required(ErrorMessage = "Invalid first name")]
+    [MinLength(2, ErrorMessage = "Enter your first name")]
     public string FirstName { get; set; } = null!;
 
 
-    
+    [DataType(DataType.Text)]
     [Display(Name = "Last name", Prompt = "Enter your last name")]
-    [Required]
+    [Required(ErrorMessage = "Invalid last name")]
+    [MinLength(2, ErrorMessage = "Enter your last name")]
     public string LastName { get; set; } = null!;
 
     [DataType(DataType.EmailAddress)]
     [Display(Name = "E-mail address", Prompt = "Enter your e-mail address")]
-    [Required]
+    [Required(ErrorMessage = "Invalid email address")]
+    [RegularExpression(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$", ErrorMessage = "Invalid email address")]
     public string Email { get; set; } = null!;
 
 
     [DataType(DataType.Password)]
     [Display(Name = "Password", Prompt = "Enter your password")]
-    [Required]
+    [Required(ErrorMessage = "Invalid email address")]
+    [RegularExpression(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]", ErrorMessage = "Invalid password")]
     public string Password { get; set; } = null!;
 
     [Required]
@@ -35,7 +39,7 @@ public class SignUpViewModel
     [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
     public string ConfirmPassword { get; set; } = null!;
 
-    [CheckboxRequired]
+    [CheckboxRequired(ErrorMessage = "You must agree to the terms & conditions.")]
     [Display(Name = "I agree to the Terms & Conditions.", Prompt = "Terms and Conditions")]
     public bool TermsAndConditions { get; set; }
 
